a34ec552304b4d9c0f8924b8c449549824951c5b
Shamanskyi <you@example.com>
2019-11-21 23:50:09 +0200

    Created! include Task component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,16 @@
+<template>
+    <ol class="list-group">
+        <task/>
+    </ol>
+</template>
+
+<script>
+    import Task from "./Task";
+
+    export default {
+        name: "Tasks",
+        components: {
+            Task
+        }
+    }
+</script>
