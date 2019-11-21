a65922d613eaccd2f96cae254d30c4ff2b28c002
Shamanskyi <you@example.com>
2019-11-21 23:47:27 +0200

    include InputGroup and Tasks components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div id="app">
-    <h1 class="text-center">Hello world</h1>
+  <div id="app" class="container">
+    <h1 class="text-center">Tasks List</h1>
+
+    <input-group class="mt-3"/>
+
+    <tasks/>
   </div>
 </template>
 
 <script>
+  import InputGroup from "./components/InputGroup";
+  import Tasks from "./components/Tasks";
+
   export default {
-    name: 'app'
+    name: 'app',
+    components: {
+      InputGroup,
+      Tasks
+    }
   }
 </script>
+
+<style>
+  #app {
+    width: 700px;
+    margin-top: 0.3em;
+  }
+</style>
